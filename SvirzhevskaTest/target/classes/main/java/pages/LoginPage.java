f7bdef068414447b14d2427f3b8cc455c4ee9ce7
Anhelina <angelina.svirzhevska@gmail.com>
2022-04-05 20:57:32 +0300

    lesson 05.04

---
+++ SvirzhevskaTest/target/classes/main/java/pages/LoginPage.java
@@ -0,0 +1,43 @@
+package pages;
+
+import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class LoginPage extends ParentPage{
+    @FindBy(xpath = ".//input[@placeholder='Username']")
+    private WebElement inputLogin;
+
+    @FindBy(xpath = ".//input[@placeholder='Password']")
+    private WebElement inputPassword;
+    @FindBy(xpath = ".//button[text()='Sign In']")
+    private WebElement buttonSignIn;
+
+    public LoginPage(WebDriver webDriver) {
+        super(webDriver);
+    }
+
+    public void openLoginPage(){
+        try {
+            webDriver.get("https://qa-complex-app-for-testing.herokuapp.com/");
+            logger.info("Login page was opened");
+        } catch (Exception e){
+            logger.error("Can not work with LoginPage");
+            Assert.fail("Can not work with LoginPage");
+            webDriver.quit();
+        }
+    }
+
+    public void enterLoginInSignIn(String login) {
+       enterTextToElement(inputLogin, login);
+    }
+
+    public void enterPasswordInSignIn(String password) {
+        enterTextToElement(inputPassword, password);
+    }
+
+    public void clickOnButtonSignIn() {
+        clickOnElement(buttonSignIn);
+    }
+}
---
+++ SvirzhevskaTest/target/classes/test/java/loginTest/loginTest.java
@@ -0,0 +1,79 @@
+package loginTest;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.io.File;
+import java.util.concurrent.TimeUnit;
+
+public class loginTest {
+
+    WebDriver webDriver;
+
+    @Test
+    public void validLogin(){
+        File fileFF = new File("./src/drivers/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", fileFF.getAbsolutePath());
+        webDriver = new ChromeDriver();
+
+        webDriver.manage().window().maximize();
+        webDriver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+        webDriver.get("https://qa-complex-app-for-testing.herokuapp.com/");
+        System.out.println("site was opened");
+
+        webDriver.findElement(By.xpath(".//input[@placeholder='Username']")).clear();
+        webDriver.findElement(By.xpath(".//input[@placeholder='Username']")).sendKeys("auto");
+        System.out.println("'auto' was inputted");
+
+        webDriver.findElement(By.xpath(".//input[@placeholder='Password']")).clear();
+        webDriver.findElement(By.xpath(".//input[@placeholder='Password']")).sendKeys("123456qwerty");
+        System.out.println("pass was inputted");
+
+        webDriver.findElement(By.xpath(".//button[text()='Sign In']")).click();
+        System.out.println("button was clicked");
+
+        Assert.assertTrue("Button 'Sign Out' is not displayed", isButtonSignOutVisible());
+        webDriver.quit();
+    }
+
+    @Test
+    public void invalidLogin(){
+        File fileFF = new File("./src/drivers/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", fileFF.getAbsolutePath());
+        webDriver = new ChromeDriver();
+
+        webDriver.manage().window().maximize();
+        webDriver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+        webDriver.get("https://qa-complex-app-for-testing.herokuapp.com/");
+        System.out.println("site was opened");
+
+        webDriver.findElement(By.xpath(".//input[@placeholder='Username']")).clear();
+        webDriver.findElement(By.xpath(".//input[@placeholder='Username']")).sendKeys("notExist");
+        System.out.println("'notExist' login was inputted");
+
+        webDriver.findElement(By.xpath(".//input[@placeholder='Password']")).clear();
+        webDriver.findElement(By.xpath(".//input[@placeholder='Password']")).sendKeys("123456qwerty");
+        System.out.println("pass was inputted");
+
+        webDriver.findElement(By.xpath(".//button[text()='Sign In']")).click();
+        System.out.println("button 'Sign In' was clicked");
+
+        Assert.assertFalse("Button 'Sign Out' is displayed", isButtonSignOutVisible());
+        webDriver.quit();
+    }
+
+    private boolean isButtonSignOutVisible() {
+        try {
+            return webDriver.findElement(By.xpath(".//button[text()='Sign Out']")).isDisplayed();
+        } catch (Exception e){
+            return false;
+        }
+
+    }
+
+}
