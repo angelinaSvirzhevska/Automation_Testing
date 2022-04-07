f06a6ab7c4586f9b7c06094bc60101ec06ffd4fd
Anhelina <angelina.svirzhevska@gmail.com>
2022-04-07 21:13:02 +0300

    lesson 05.04

---
+++ SvirzhevskaTest/src/test/java/postTest/CreatePostTest.java
@@ -0,0 +1,29 @@
+package postTest;
+
+import baseTest.BaseTest;
+import libs.TestData;
+import libs.Util;
+import org.junit.Test;
+
+public class CreatePostTest  extends BaseTest {
+    private static final String POST_BODY = "Angelina Body of post" + Util.getDateAndTimeFormated();
+    final String POST_TITLE = "Angelina Title of post " + Util.getDateAndTimeFormated();
+
+    @Test
+    public void createPost(){
+
+        loginPage
+                .loginWithValidCred()
+                .checkIsButtonSignOutVisible()
+                .clickOnButtonCreatePost()
+                .checkIsInputTitlePresent()
+                .enterTextIntoInputTitle(POST_TITLE)
+                .enterTextIntoInputBody(POST_BODY)
+                .clickOnTheSaveButton()
+                ;
+
+
+
+
+    }
+}
